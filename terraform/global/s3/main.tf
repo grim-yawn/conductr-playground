bb6679c277cc9becd420a1775e83a5da9f761693
Ivan Kosolapov <ikosolapov@express42.com>
2018-09-22 17:57:27 +0300

    Add bucket for backend

---
+++ terraform/global/s3/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  backend "gcs" {
+    bucket = "harbinger-212217-state"
+    prefix = "global/s3"
+  }
+}
+
+provider "google" {
+  version = "1.18.0"
+
+  project = "habringer-212217"
+  region  = "us-east1-b"
+}
+
+resource "google_storage_bucket" "state_storage" {
+  name = "harbinger-212217-state"
+}
