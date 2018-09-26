05bf5d793d4d3f2a4a5ee72205138021dbf35778
Ivan Kosolapov <ikosolapov@express42.com>
2018-09-27 01:34:44 +0300

    Automate haproxy bundles running

--- terraform/stage/variables.tf
+++ terraform/stage/variables.tf
@@ -14,5 +14,5 @@ variable "conductr_machine_type" {
 
 variable "conductr_base_image" {
   description = "Base image for conductr instances"
-  default     = "conductr-base-1537978364"
+  default     = "conductr-base-1537997377"
 }
